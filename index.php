06b347f8ddd8e72e1e35b93dbb08302a2c070cef
dmitkorob <dimakorobejnikoff@yandex.ru>
2021-04-09 22:56:48 +0300

    Доработка с рекурсивным поиском

--- index.php
+++ index.php
@@ -19,70 +19,85 @@ function logs ($string) {
 	fclose($open_log);
 }
 
-$host = 'localhost';
-$login = 'register';
-$password = 'C1bwo8SOHFZoH56T';
-$db_name = 'parser';
 
-$file = "import/YYYY/MM/DD";
-// $explode = explode('/', $file);
-// foreach($explode as $val) {
-// 	$way .= $val.'/';
-// 	mkdir($way);
-// }
+function scan($dir) {
+	$scan = scandir($dir);
+	$scan = array_diff($scan, array('.', '..'));
+	sort($scan);
+	foreach($scan as $val) {
+		$way = $dir;
+		if(is_file($way.'/'.$val)) {
+			$way .='/'.$val;
+			echo "<pre>";
+			echo $way."<br>";
+			pars($val, $way);
+		} else if(is_dir($way.'/'.$val)) {
+			$way .='/'.$val;
+			$mass[$val] = scan($way);
+		}
+	}
 
+}
 
-logs('Начало сканирования');
-$scandir = scandir($file);
-$scandir = array_diff($scandir, ['.', '..']);
-sort($scandir);
-if(!empty($scandir)) {
+function pars($file, $way) {
+
+	$host = 'localhost';
+	$login = 'register';
+	$password = 'C1bwo8SOHFZoH56T';
+	$db_name = 'parser';
+
+	logs('Начало сканирования');
 	$conn = mysqli_connect($host, $login, $password);
 	if(!empty($conn)) {
 		logs('Подключение к PhpMyAdmin');
 	}
 	$create_db = mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS $db_name");
 	mysqli_close($conn);
-	if(!empty($create_bd)) {
+	if(isset($create_bd)) {
 		logs('Coздана база данных '.$db_name);
 	}
 	$connect = mysqli_connect($host, $login, $password, $db_name);
-	foreach($scandir as $scanfile) {
-		if(substr($scanfile, -4) == '.csv'){
-			$create = mysqli_query($connect, "CREATE TABLE `{$scanfile}` ( 
-				ID int PRIMARY KEY AUTO_INCREMENT, 
-				REG_NUMB varchar(256) NOT NULL, 
-				NAME varchar(256) NOT NULL, 
-				URL varchar(256) NOT NULL, 
-				TEL_NUMB varchar(256) NOT NULL, 
-				EMAIL varchar(256) NOT NULL 
-			)");
-			if(!empty($create)) {
-				logs('Coздана таблица '.$scanfile);
-			}
-			$fopen = fopen($file.'/'.$scanfile, 'r');
-			$count = 0;
-			while($str = fgets($fopen)) {
-				$col = explode(',', $str);
-				if(!empty($create)) {
-					$insert = mysqli_query($connect, "INSERT INTO `{$scanfile}` VALUES(
-						NULL,
-						'$col[0]',
-						'$col[1]',
-						'$col[2]', 
-						'$col[3]',
-						'$col[4]'
-					)");	
-				}
-				if(!empty($insert)) {
-					$count++;
-					logs('Добавлена запись №'.$count);
-				}
-			}
-			fclose($fopen);
+	if(substr($file, -4) == '.csv'){
+		$create = mysqli_query($connect, "CREATE TABLE `{$file}` ( 
+			ID int PRIMARY KEY AUTO_INCREMENT, 
+			REG_NUMB varchar(256) NOT NULL, 
+			NAME varchar(256) NOT NULL, 
+			URL varchar(256) NOT NULL, 
+			TEL_NUMB varchar(256) NOT NULL, 
+			EMAIL varchar(256) NOT NULL 
+		)");
+		if(isset($create)) {
+			logs('Coздана таблица '.$file);
 		}
+		$fopen = fopen($way, 'r');
+		$count = 0;
+		while($str = fgets($fopen)) {
+			$col = explode(',', $str);
+			if(isset($create)) {
+				$insert = true;
+				// $insert = mysqli_query($connect, "INSERT INTO `{$file}` VALUES(
+				// 	NULL,
+				// 	'$col[0]',
+				// 	'$col[1]',
+				// 	'$col[2]', 
+				// 	'$col[3]',
+				// 	'$col[4]'
+				// )");	
+			}
+			if(isset($insert)) {
+				$count++;
+				logs('Добавлена запись №'.$count);
+			}
+		}
+		fclose($fopen);
 	}
+	mysqli_close($connect);
+	logs('Завершение сканирования');
 }
-logs('Завершение сканирования');
+
+
+$dir = "import";
+scan($dir);
+echo $count_file;
 
 ?>
